11ac360b8962015483eac0191174b6c11ed2c8dd
yuanhongyu <yuanhongyu@jianhuabm.com>
2018-10-27 11:08:50 +0800

    修改权限

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:amap_location/amap_location.dart';
 import 'package:async_loader/async_loader.dart';
 //import 'package:simple_permissions/simple_permissions.dart';
-import 'package:easy_alert/easy_alert.dart';
+//import 'package:easy_alert/easy_alert.dart';
 
 
 void main() {
